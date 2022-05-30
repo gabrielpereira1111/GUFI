71a7ef1a3d082b9f0226fb4d0a7479bd6ea209d0
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-30 15:24:57 -0300

    Adiciona DataAnnotations à Domain de Presenças

--- gufi_webApi/gufi_webApi/Domains/Presenca.cs
+++ gufi_webApi/gufi_webApi/Domains/Presenca.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace gufi_webApi.Domains
 {
     public partial class Presenca
     {
         public int IdPresenca { get; set; }
+        [Required(ErrorMessage = "Informe o usuário!")]
         public int? IdUsuario { get; set; }
+        [Required(ErrorMessage = "Informe o evento!")]
         public int? IdEvento { get; set; }
+        [Required(ErrorMessage = "Informe a situação!")]
         public int? IdSituacao { get; set; }
 
         public virtual Evento? IdEventoNavigation { get; set; }
