4ebe45ba36992d7685c7e2304cb6a294cdbf6f4c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-30 15:48:23 -0300

    Adiciona DataAnnotations à Domain de Instituição

--- gufi_webApi/gufi_webApi/Domains/Instituicao.cs
+++ gufi_webApi/gufi_webApi/Domains/Instituicao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace gufi_webApi.Domains
 {
@@ -11,8 +12,11 @@ namespace gufi_webApi.Domains
         }
 
         public int IdInstituicao { get; set; }
+        [Required(ErrorMessage = "Informe o CNPJ da instituição")]
         public string Cnpj { get; set; } = null!;
+        [Required(ErrorMessage = "Informe o nome fantasia da instituição")]
         public string NomeFantasia { get; set; } = null!;
+        [Required(ErrorMessage = "Informe o endereço da instituição")]
         public string Endereco { get; set; } = null!;
 
         public virtual ICollection<Evento> Eventos { get; set; }
