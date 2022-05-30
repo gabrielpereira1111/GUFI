2c9499028ef0ba8107be4d1b09b7d937575f629b
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-30 15:07:18 -0300

    Adiciona DataAnnotations à Domain de Usuários

--- gufi_webApi/gufi_webApi/Domains/Usuario.cs
+++ gufi_webApi/gufi_webApi/Domains/Usuario.cs
@@ -12,9 +12,16 @@ namespace gufi_webApi.Domains
         }
 
         public int IdUsuario { get; set; }
+        [Required(ErrorMessage = "Informe o tipo desse usuário!")]
         public int? IdTipoUsuario { get; set; }
-        public string? NomeUsuario { get; set; }
+        [Required(ErrorMessage = "Informe o nome do usuário!")]
+        public string NomeUsuario { get; set; } = null!;
+        [Required(ErrorMessage = "Informe o email do usuário!")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Informe a senha do usuário!")]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "A senha deve ter, no mínimo, 10 caracteres e, no máximo, 20 caracteres")]
+        [DataType(DataType.Password)]
         public string Senha { get; set; } = null!;
 
         public virtual TipoUsuario? IdTipoUsuarioNavigation { get; set; }
