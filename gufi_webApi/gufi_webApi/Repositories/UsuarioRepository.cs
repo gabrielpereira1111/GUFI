00481cffd9a8d538b8085665ad8675d108ca31fe
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-30 16:00:51 -0300

    Adiciona a repository de usuário

---
+++ gufi_webApi/gufi_webApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using gufi_webApi.Context;
+using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+
+namespace gufi_webApi.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        GufiContext ctx = new GufiContext();
+        public Usuario Login(string email, string senha)
+        {
+            return ctx.Usuarios.FirstOrDefault(x => x.Email == email && x.Senha == senha);
+        }
+    }
+}
