7a103c5a2b1ceecc728f9b68095ab9b8261b6986
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-30 15:15:49 -0300

    Adiciona DataAnnotations à Domain de Eventos

--- gufi_webApi/gufi_webApi/Domains/Evento.cs
+++ gufi_webApi/gufi_webApi/Domains/Evento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace gufi_webApi.Domains
 {
@@ -11,11 +12,18 @@ namespace gufi_webApi.Domains
         }
 
         public int IdEvento { get; set; }
+        [Required(ErrorMessage = "Informe o tipo de evento!")]
         public int? IdTipoEvento { get; set; }
+        [Required(ErrorMessage = "Informe a instituição responsável pelo evento!")]
         public int? IdInstituicao { get; set; }
+        [Required(ErrorMessage = "Informe o nome do evento!")]
         public string NomeEvento { get; set; } = null!;
+        [Required(ErrorMessage = "Informe a descrição do evento!")]
         public string Descricao { get; set; } = null!;
+        [Required(ErrorMessage = "Informe a data do evento!")]
+        [DataType(DataType.DateTime)]
         public DateTime DataEvento { get; set; }
+        [Required(ErrorMessage = "Informe se o evento é público ou privado!")]
         public bool? AcessoLivre { get; set; }
 
         public virtual Instituicao? IdInstituicaoNavigation { get; set; }
