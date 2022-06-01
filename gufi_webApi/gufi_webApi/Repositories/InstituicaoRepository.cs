dd76324c936e01227b3205a6c63c389ca725bcf0
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 15:16:47 -0300

    Adiciona repository de instituicao

---
+++ gufi_webApi/gufi_webApi/Repositories/InstituicaoRepository.cs
@@ -0,0 +1,16 @@
+﻿using gufi_webApi.Context;
+using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+
+namespace gufi_webApi.Repositories
+{
+    public class InstituicaoRepository : IInstituicaoRepository
+    {
+        GufiContext ctx = new GufiContext();
+        public void Create(Instituicao instituicao)
+        {
+            ctx.Instituicaos.Add(instituicao);
+            ctx.SaveChanges();
+        }
+    }
+}
