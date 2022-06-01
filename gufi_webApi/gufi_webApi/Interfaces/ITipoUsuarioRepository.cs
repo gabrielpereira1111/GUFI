8b5ebab008b1867ad80ca2105f26dad74726300c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 12:54:21 -0300

    Adiciona interfce e tipos de usuario

---
+++ gufi_webApi/gufi_webApi/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using gufi_webApi.Domains;
+
+namespace gufi_webApi.Interfaces
+{
+    public interface ITipoUsuarioRepository
+    {
+        void Create(TipoUsuario tipoUsuario);
+        List<TipoUsuario> ReadAll();
+        TipoUsuario GetById(int id);
+        void Update(TipoUsuario tipoUsuario, int idTipoUsuario);
+        void Delete(int id);
+
+    }
+}
