bb10f16b86a444ee35c38128a5dbf5b5029619f1
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 14:29:24 -0300

    Adiciona interface de tipo de evento

---
+++ gufi_webApi/gufi_webApi/Interfaces/ITiposEventoRepository.cs
@@ -0,0 +1,14 @@
+﻿using gufi_webApi.Domains;
+
+namespace gufi_webApi.Interfaces
+{
+    public interface ITiposEventoRepository
+    {
+        void Create(TipoEvento tipoEvento);
+        List<TipoEvento> ReadAll();
+        TipoEvento GetById(int id);
+        void Update(TipoEvento tipoEvento, int id);
+        void Delete(int id);
+        void Delete(string nomeTipoEvento);
+    }
+}
