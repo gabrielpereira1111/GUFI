6693a331b67fb4325560483ea241bc7b977d3e69
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 12:59:43 -0300

    Adiciona Repositories de tipos de usuário

---
+++ gufi_webApi/gufi_webApi/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,36 @@
+﻿using gufi_webApi.Context;
+using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+
+namespace gufi_webApi.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+        GufiContext ctx = new GufiContext();
+
+        public void Create(TipoUsuario tipoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoUsuario GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoUsuario> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TipoUsuario tipoUsuario, int idTipoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
