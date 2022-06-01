f41bc1efa97fa05b5e01fe53552a5453e812eb47
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 15:21:04 -0300

    Adiciona controller de instituicao

---
+++ gufi_webApi/gufi_webApi/Controllers/InstituicaoController.cs
@@ -0,0 +1,34 @@
+﻿using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+using gufi_webApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gufi_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstituicaoController : ControllerBase
+    {
+        private IInstituicaoRepository _instituicaoRepository { get; set; } = null!;
+        public InstituicaoController()
+        {
+            _instituicaoRepository = new InstituicaoRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Instituicao instituicao)
+        {
+            try
+            {
+                _instituicaoRepository.Create(instituicao);
+                return StatusCode(201);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+    }
+}
