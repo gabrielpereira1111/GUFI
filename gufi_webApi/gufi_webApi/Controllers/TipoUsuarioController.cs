ba1af492f8080312681d2ad8d65305359d5dc09c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 13:11:50 -0300

    Adiciona método de listar no controller de tipos de usuário

---
+++ gufi_webApi/gufi_webApi/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using gufi_webApi.Interfaces;
+using gufi_webApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gufi_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoUsuarioController : ControllerBase
+    {
+        private ITipoUsuarioRepository _tipoUsuarioRepository { get; set; } = null!
+        public TipoUsuarioController()
+        {
+            _tipoUsuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ReadAll()
+        {
+            try
+            {
+                return Ok(_tipoUsuarioRepository.ReadAll());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+    }
+}
