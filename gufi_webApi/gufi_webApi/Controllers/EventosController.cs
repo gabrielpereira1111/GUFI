2cef13cdf353f1a2de4900e7f9901b5babfec34c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 15:54:49 -0300

    Adiciona métodos de leitura no controller de eventos

---
+++ gufi_webApi/gufi_webApi/Controllers/EventosController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using gufi_webApi.Interfaces;
+using gufi_webApi.Repositories;
+using gufi_webApi.Domains;
+
+namespace gufi_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventosController : ControllerBase
+    {
+        private IEventoRepository _eventoRepository { get; set; } = null!;
+        public EventosController()
+        {
+            _eventoRepository = new EventoRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_eventoRepository.ReadAll());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                Evento eventoBuscado = _eventoRepository.GetById(id);
+                if (eventoBuscado != null)
+                {
+                    return Ok(eventoBuscado);
+                }
+                return NotFound("Evento não encontrado!");
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+    }
+}
