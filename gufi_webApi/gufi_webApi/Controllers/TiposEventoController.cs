fc828cd95c601a4e085b4916b3a8cddd1e9d14f9
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-06-01 15:10:41 -0300

    Adiciona Repository e Controller de tipos de evento

---
+++ gufi_webApi/gufi_webApi/Controllers/TiposEventoController.cs
@@ -0,0 +1,89 @@
+﻿using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+using gufi_webApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gufi_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposEventoController : ControllerBase
+    {
+        private ITiposEventoRepository _tiposEventosRepository { get; set; } = null!;
+        public TiposEventoController()
+        {
+            _tiposEventosRepository = new TiposEventoRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ReadAll()
+        {
+            try
+            {
+                return Ok(_tiposEventosRepository.ReadAll());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                TipoEvento tipoEventoBuscado = _tiposEventosRepository.GetById(id);
+                return Ok(tipoEventoBuscado);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+
+        [HttpDelete("deletarporid/{id}")]
+        public IActionResult DeleteId(int id)
+        {
+            try
+            {
+                _tiposEventosRepository.Delete(id);
+                return NoContent();
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+
+        [HttpDelete("deletarpornome/{nomeTipoEvento}")]
+        public IActionResult DeleteNome(string nomeTipoEvento)
+        {
+            try
+            {
+                _tiposEventosRepository.Delete(nomeTipoEvento);
+                return NoContent();
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(TipoEvento tipoEvento)
+        {
+            try
+            {
+                _tiposEventosRepository.Create(tipoEvento);
+                return Ok();
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+    }
+}
---
+++ gufi_webApi/gufi_webApi/Repositories/TiposEventoRepository.cs
@@ -0,0 +1,50 @@
+﻿using gufi_webApi.Context;
+using gufi_webApi.Domains;
+using gufi_webApi.Interfaces;
+
+namespace gufi_webApi.Repositories
+{
+    public class TiposEventoRepository : ITiposEventoRepository
+    {
+        GufiContext ctx = new GufiContext();
+        public void Create(TipoEvento tipoEvento)
+        {
+            ctx.TipoEventos.Add(tipoEvento);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            TipoEvento tipoEvento = GetById(id);
+            ctx.Remove(tipoEvento);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(string nomeTipoEvento)
+        {
+            TipoEvento tipoEventoBuscado = ctx.TipoEventos.First(p => p.TituloTipoEvento == nomeTipoEvento);
+            ctx.Remove(tipoEventoBuscado);
+            ctx.SaveChanges();
+        }
+
+        public TipoEvento GetById(int id)
+        {
+            TipoEvento tipoEvento = ctx.TipoEventos.First(p => p.IdTipoEvento == id);
+            if(tipoEvento != null)
+            {
+                return tipoEvento;
+            }
+            return null;
+        }
+
+        public List<TipoEvento> ReadAll()
+        {
+            return ctx.TipoEventos.ToList();
+        }
+
+        public void Update(TipoEvento tipoEvento, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
